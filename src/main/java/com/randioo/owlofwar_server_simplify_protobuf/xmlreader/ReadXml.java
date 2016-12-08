fd8b42e9175c59fe1b21f6f2a7889e845f423c16
wcy <1101697681@qq.com>
2016-12-08 14:25:03 +0800

    change ReadXml

--- src/main/java/com/randioo/owlofwar_server_simplify_protobuf/xmlreader/ReadXml.java
+++ src/main/java/com/randioo/owlofwar_server_simplify_protobuf/xmlreader/ReadXml.java
@@ -2,12 +2,14 @@ package com.randioo.owlofwar_server_simplify_protobuf.xmlreader;
 
 public class ReadXml {
 	public static void readAll() {
-		CardXmlReader.readXml("./xml/Card.xml");
-		AIPlayer001Reader.readXml("./xml/AIPlayer001.xml");
-		CardLvXmlReader.readXml("./xml/CardLevel.xml");
-		ExtraCardXmlReader.readXml("./xml/ExtraCard.xml");
-		MapsXmlReader.readXml("./xml/Maps.xml");
-		AutoNpcXmlReader.readXml("./xml/AutoNPC.xml");
-		CardInitXmlReader.readXml("./xml/CardInit.xml");
+		System.out.println("load xml");
+		CardXmlReader.readXml("./config/Card.xml");
+		AIPlayer001Reader.readXml("./config/AIPlayer001.xml");
+		CardLvXmlReader.readXml("./config/CardLevel.xml");
+		ExtraCardXmlReader.readXml("./config/ExtraCard.xml");
+		MapsXmlReader.readXml("./config/Maps.xml");
+		AutoNpcXmlReader.readXml("./config/AutoNPC.xml");
+		CardInitXmlReader.readXml("./config/CardInit.xml");
+		System.out.println("load xml complete");
 	}
 }
